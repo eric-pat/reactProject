c1620fd27e1e13d3e24927afc397a3d2204b5451
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-09-07 17:32:14 +0200

    react basics 04

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    const pokemon = pokemonList[0]; // Changez l'index pour choisir le Pokémon
 
-  return (
-    <>
-      <div>
-       <PokemonCard/>
-      </div>
-    </>
-  )
+    return (
+        <div>
+            <PokemonCard pokemon={pokemon} />
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
-
-function PokemonCard() {
-
-    const pokemon = pokemonList[1];//modifier le chiffre pour changer de pokemon
+function PokemonCard(props) {
 
     return (
         <figure>
-            {pokemon.imgSrc ? (
-                <img src={pokemon.imgSrc} alt={pokemon.name} />
+            {props.pokemon.imgSrc ? (
+                <img src={props.pokemon.imgSrc} alt={props.pokemon.name} />
             ) : (
                 <p>???</p>
             )}
-            <figcaption>{pokemon.name}</figcaption>
+            <figcaption>{props.pokemon.name}</figcaption>
         </figure>
     );
 }
 
 export default PokemonCard;
-
-
